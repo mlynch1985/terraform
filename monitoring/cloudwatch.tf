6aea47fdbe10a63a05d11c61439728bac9102f2c
awsml <awsml@amazon.com>
2020-05-19 16:51:14 -0400

    Added CWA Alarms

--- monitoring/cloudwatch.tf
+++ monitoring/cloudwatch.tf
@@ -1,4 +1,4 @@
-## Define Cloudwatch EC2 Events
+## Alert on AutoScaling Events and send to SNS Topic
 resource "aws_cloudwatch_event_rule" "cw-asg-events" {
   name        = "${var.namespace}-cw-asg-events"
   description = "Notify for AutoScaling Event"
@@ -20,8 +20,49 @@ resource "aws_cloudwatch_event_rule" "cw-asg-events" {
 EOF
 }
 
+## Define which SNS Topic to push to
 resource "aws_cloudwatch_event_target" "cw-asg-events-targets" {
-    target_id = "sns-topic-asg-events"
-    rule = aws_cloudwatch_event_rule.cw-asg-events.name
-    arn = aws_sns_topic.sns-topic-asg-events.arn
+  target_id = "sns-topic-asg-events"
+  rule      = aws_cloudwatch_event_rule.cw-asg-events.name
+  arn       = aws_sns_topic.sns-topic-asg-events.arn
+}
+
+## Create Metric Alarm for High CPU
+resource "aws_cloudwatch_metric_alarm" "cw-alarm-ec2-highcpu" {
+  alarm_name          = "${var.namespace}-ec2-high-cpu"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPU_USAGE_IDLE"
+  namespace           = "CWAgent"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "20"
+  alarm_description   = "This metric monitors ec2 cpu utilization"
+  alarm_actions       = ["${aws_cloudwatch_event_target.cw-asg-events-targets.arn}"]
+
+  tags = {
+    Name        = "${var.namespace}-cw-alarm-ec2-highcpu"
+    Environment = var.environment
+    Namespace   = var.namespace
+  }
+}
+
+## Create Metric Alarm for Disk Space
+resource "aws_cloudwatch_metric_alarm" "cw-alarm-ec2-diskspace" {
+  alarm_name          = "${var.namespace}-ec2-disk-space"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "DISK_FREE"
+  namespace           = "CWAgent"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "5000000000" ## 5 GB
+  alarm_description   = "This metric monitors ec2 free disk space"
+  alarm_actions       = ["${aws_cloudwatch_event_target.cw-asg-events-targets.arn}"]
+
+  tags = {
+    Name        = "${var.namespace}-cw-alarm-ec2-diskspace"
+    Environment = var.environment
+    Namespace   = var.namespace
+  }
 }
--- monitoring/sns.tf
+++ monitoring/sns.tf
@@ -22,7 +22,10 @@ data "aws_iam_policy_document" "sns-iam-asg-events" {
 
     principals {
       type        = "Service"
-      identifiers = ["events.amazonaws.com"]
+      identifiers = [
+        "events.amazonaws.com",
+        "cloudwatch.amazonaws.com"
+      ]
     }
 
     resources = ["${aws_sns_topic.sns-topic-asg-events.arn}"]
