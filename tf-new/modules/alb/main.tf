90b3a80dd26f740783f6b7fe5b4b1700dec10dbf
Mike Lynch <mlynch1985@gmail.com>
2020-10-29 23:01:17 -0400

    updated healthcheck temporarily for testing

--- tf-new/modules/alb/main.tf
+++ tf-new/modules/alb/main.tf
@@ -28,7 +28,7 @@ resource "aws_lb_target_group" "this" {
     timeout             = 5
     healthy_threshold   = 3
     unhealthy_threshold = 2
-    matcher             = "200-399"
+    matcher             = "200-403"
   }
 
   tags = merge(
--- tf-new/userdata.sh
+++ tf-new/userdata.sh
@@ -4,5 +4,3 @@ yum update -y
 yum install httpd -y
 service httpd start
 chkconfig httpd on
-
-echo "Hello World!" >> /var/www/html/index.html
