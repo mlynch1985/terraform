c33c52e9ea565a062e86c496bdf18da1ef1042ff
awsml <awsml@amazon.com>
2020-12-16 22:08:52 -0500

    fixed bug in elb module

--- modules/elb/main.tf
+++ modules/elb/main.tf
@@ -6,13 +6,13 @@ resource "aws_lb" "this" {
   subnets            = var.subnets
 
   ## ALB
-  security_groups            = var.load_balancer_type == "ALB" ? var.security_groups : null
-  drop_invalid_header_fields = var.load_balancer_type == "ALB" ? var.drop_invalid_header_fields : null
-  idle_timeout               = var.load_balancer_type == "ALB" ? var.idle_timeout : null
-  enable_http2               = var.load_balancer_type == "ALB" ? var.enable_http2 : null
+  security_groups            = var.load_balancer_type == "application" ? var.security_groups : null
+  drop_invalid_header_fields = var.load_balancer_type == "application" ? var.drop_invalid_header_fields : null
+  idle_timeout               = var.load_balancer_type == "application" ? var.idle_timeout : null
+  enable_http2               = var.load_balancer_type == "application" ? var.enable_http2 : null
 
   ## NLB
-  enable_cross_zone_load_balancing = var.load_balancer_type == "NLB" ? var.enable_cross_zone_load_balancing : null
+  enable_cross_zone_load_balancing = var.load_balancer_type == "network" ? var.enable_cross_zone_load_balancing : null
 
   ## Optional
   enable_deletion_protection = var.enable_deletion_protection
