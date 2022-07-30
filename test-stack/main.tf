058c021744262f832bf01dffba9f2119bc680318
Mike Lynch <mlynch1985@gmail.com>
2022-07-30 15:19:30 -0400

    improved test-stack to bring same httpd website online for asg

--- test-stack/main.tf
+++ test-stack/main.tf
@@ -43,50 +43,6 @@ data "aws_ami" "amazonlinux2" {
   }
 }
 
-module "asg" {
-  source = "github.com/mlynch1985/terraform/custom-modules-examples/asg/"
-
-  image_id               = data.aws_ami.amazonlinux2.id
-  instance_type          = "c5.xlarge"
-  server_name            = "app_server"
-  vpc_security_group_ids = [module.vpc.default_security_group.id]
-  kms_key_arn            = module.kms_key_asg.arn
-  subnets                = module.vpc.private_subnets[*].id
-
-  user_data                = null
-  iam_instance_profile     = module.iam_role.profile
-  min_size                 = 1
-  max_size                 = 3
-  healthcheck_grace_period = 300
-  healthcheck_type         = "EC2"
-  desired_capacity         = 3
-  target_group_arns        = []
-
-  block_device_mappings = [
-    {
-      device_name : "/dev/xvda"
-      volume_type : "gp3"
-      volume_size : "50"
-      iops : null
-      delete_on_termination : true
-    },
-    {
-      device_name : "xvdf"
-      volume_type : "gp3"
-      volume_size : "100"
-      iops : null
-      delete_on_termination : true
-    }
-  ]
-}
-
-module "iam_role" {
-  source = "github.com/mlynch1985/terraform/custom-modules-examples/iam_role/"
-
-  service   = "ec2"
-  role_name = "use1_dev_ec2_servers"
-}
-
 module "ipam" {
   source = "github.com/mlynch1985/terraform/custom-modules-examples/ipam/"
 
@@ -97,6 +53,71 @@ module "ipam" {
   allocation_min_netmask_length     = 16
   allocation_max_netmask_length     = 28
   ipam_cidr                         = "10.0.0.0/8"
+}
+
+/* ToDo: Integrate IPAM with VPC Module */
+module "vpc" {
+  source = "github.com/mlynch1985/terraform/custom-modules-examples/vpc/"
+
+  namespace            = var.namespace
+  environment          = var.environment
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  enable_flow_logs     = true
+  subnet_size_offset   = 8
+  target_az_count      = 3
+  tgw_id               = ""
+  vpc_type             = "hub"
+}
+
+/* ToDo: Implement ManagedPolicy ARNs and Inline Policy Attachements */
+module "iam_role" {
+  source = "github.com/mlynch1985/terraform/custom-modules-examples/iam_role/"
+
+  service   = "ec2"
+  role_name = "${var.namespace}_${var.environment}_ec2"
+}
+
+resource "aws_iam_role_policy_attachment" "ssm-agent" {
+  role       = module.iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch-agent" {
+  role       = module.iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
+/* ToDo: Create as a reusable module */
+resource "aws_security_group" "asg" {
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  name_prefix = "${var.namespace}-${var.environment}-ec2"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = module.vpc.vpc.id
+
+  ingress {
+    description = "Allow inbound HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.namespace}-${var.environment}-ec2"
+  }
 }
 
 module "kms_key_asg" {
@@ -123,17 +144,50 @@ module "s3_bucket" {
   iam_roles   = [module.iam_role.arn]
 }
 
-module "vpc" {
-  source = "github.com/mlynch1985/terraform/custom-modules-examples/vpc/"
+module "asg" {
+  source = "github.com/mlynch1985/terraform/custom-modules-examples/asg/"
 
-  namespace            = "use1"
-  environment          = "dev"
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-  enable_flow_logs     = true
-  subnet_size_offset   = 8
-  target_az_count      = 3
-  tgw_id               = ""
-  vpc_type             = "hub"
+  image_id               = data.aws_ami.amazonlinux2.id
+  instance_type          = "c5.xlarge"
+  server_name            = "app_server"
+  vpc_security_group_ids = [module.vpc.default_security_group.id, aws_security_group.asg.id]
+  kms_key_arn            = module.kms_key_asg.arn
+  subnets                = module.vpc.private_subnets[*].id
+
+  user_data = base64encode(<<-EOF
+#!/bin/bash
+yum upgrade -y
+yum install -y httpd
+echo "Hello World from $(hostname -f)" > /var/www/html/index.html
+service httpd start
+chkconfig httpd on
+EOF
+  )
+
+  iam_instance_profile     = module.iam_role.profile
+  min_size                 = 1
+  max_size                 = 3
+  healthcheck_grace_period = 300
+  healthcheck_type         = "EC2"
+  desired_capacity         = 3
+  target_group_arns        = []
+
+  block_device_mappings = [
+    {
+      device_name : "/dev/xvda"
+      volume_type : "gp3"
+      volume_size : "50"
+      iops : null
+      delete_on_termination : true
+    },
+    {
+      device_name : "xvdf"
+      volume_type : "gp3"
+      volume_size : "100"
+      iops : null
+      delete_on_termination : true
+    }
+  ]
 }
+
+/* ToDo: Implement public load balancers */
