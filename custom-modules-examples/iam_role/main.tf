416ce95680d3c6e6b9d5884d9bf75b57ccd58dc4
Mike Lynch <mlynch1985@gmail.com>
2022-07-31 00:34:04 -0400

    squashing bugs

--- custom-modules-examples/iam_role/main.tf
+++ custom-modules-examples/iam_role/main.tf
@@ -11,12 +11,12 @@ data "aws_iam_policy_document" "trust_policy" {
 resource "aws_iam_role" "this" {
   assume_role_policy    = data.aws_iam_policy_document.trust_policy.json
   force_detach_policies = true
-  name_prefix           = var.role_name
+  name_prefix           = "${var.role_name}_"
 }
 
 resource "aws_iam_instance_profile" "this" {
-  name = aws_iam_role.this.name
-  role = aws_iam_role.this.id
+  name_prefix = "${var.role_name}_"
+  role        = aws_iam_role.this.id
 }
 
 resource "aws_iam_policy" "inline" {
