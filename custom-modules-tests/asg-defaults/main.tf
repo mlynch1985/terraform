f77bac70f32551f7e488e3152ceb62dd5719d8ec
Mike Lynch <mlynch1985@gmail.com>
2022-12-17 12:40:37 -0500

    clearing checkov error

--- custom-modules-tests/asg-defaults/main.tf
+++ custom-modules-tests/asg-defaults/main.tf
@@ -1,9 +1,11 @@
 resource "aws_security_group" "tester" {
+  #checkov:skip=CKV2_AWS_5:Ensure that Security Groups are attached to another resource
   name_prefix = "asg_tester_"
   description = "SG Used to test the ASG Terraform Module"
   vpc_id      = var.vpc_id
 
   egress {
+    description      = "Allow all outbound"
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
