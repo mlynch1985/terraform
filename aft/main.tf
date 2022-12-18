cc88566156d04ad02af444a3c4351c08a798e6b8
Mike Lynch <mlynch1985@gmail.com>
2022-12-17 20:27:58 -0500

    commenting out AFT module to clear security checks

--- aft/main.tf
+++ aft/main.tf
@@ -31,21 +31,21 @@ provider "aws" {
 
 data "aws_caller_identity" "current" {}
 
-# module "control_tower_account_factory" {
-module "aft-initiator" {
-  # source = "git@github.com:aws-ia/terraform-aws-control_tower_account_factory.git"
-  source  = "aws-ia/control_tower_account_factory/aws"
-  version = "1.7.0"
+# # module "control_tower_account_factory" {
+# module "aft-initiator" {
+#   # source = "git@github.com:aws-ia/terraform-aws-control_tower_account_factory.git"
+#   source  = "aws-ia/control_tower_account_factory/aws"
+#   version = "1.7.0"
 
-  aft_feature_cloudtrail_data_events      = false
-  aft_feature_delete_default_vpcs_enabled = true
-  aft_feature_enterprise_support          = false
-  aft_management_account_id               = "067521573221"
-  aft_metrics_reporting                   = true
-  audit_account_id                        = "203495621194"
-  cloudwatch_log_group_retention          = 30
-  ct_home_region                          = var.region
-  ct_management_account_id                = data.aws_caller_identity.current.account_id
-  log_archive_account_id                  = "890487267480"
-  tf_backend_secondary_region             = "us-east-1"
-}
+#   aft_feature_cloudtrail_data_events      = false
+#   aft_feature_delete_default_vpcs_enabled = true
+#   aft_feature_enterprise_support          = false
+#   aft_management_account_id               = "067521573221"
+#   aft_metrics_reporting                   = true
+#   audit_account_id                        = "203495621194"
+#   cloudwatch_log_group_retention          = 30
+#   ct_home_region                          = var.region
+#   ct_management_account_id                = data.aws_caller_identity.current.account_id
+#   log_archive_account_id                  = "890487267480"
+#   tf_backend_secondary_region             = "us-east-1"
+# }
